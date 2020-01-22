2d2059b9ebc062b684133e2bf392952a15876ba4
bension10 <bension10@gmail.com>
2020-01-22 19:48:35 +0800

    configure reducer, actions, api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './reducers';
 import './index.css';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
